a0f9db53eaaff6b208362501db294a6ea472a9d3
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-13 14:56:13 +0100

    Chapter 3 describe initialized constant error

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
-require "oystercard"
-
+# Uninitialized constant Oystercard (NameError)
+# ./spec/oystercard_spec.rb:2:in `<top (required)>'
+# line 2
+# Create file and create class Oystercard link to spec file
 describe Oystercard do
-  
+
 end
