d5d5bed8461921509fe53854c6408f5c3bc45412
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-13 15:43:50 +0100

    Chapter 6 added UPPER_LIMIT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   attr_reader :balance
+  UPPER_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    fail "Balance cannot exceed #{UPPER_LIMIT}" if (@balance += amount) > UPPER_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,11 +20,23 @@ describe Oystercard do
     it 'can top_up' do
       expect(subject.top_up(5)).to eq subject.balance
     end
+
+    it "cannot top up past upper_limit" do
+      oystercard = Oystercard.new(85)
+      expect{ oystercard.top_up(10) }.to raise_error "Balance cannot exceed #{Oystercard::UPPER_LIMIT}"
+    end
   end
-  
+
 end
 
 # initialize an instance of the Oystercard class, balance = 0/default
-# call top_up(with an arguement) on instance of Oystercard (oystercard)
+# call top_up(with an argument) on instance of Oystercard (oystercard)
 # FAIL
 # call balance on oystercard and check balance has increased to argument
+
+# initialize an instance of the Oystercard class, balance = 0
+# call top_up(>90) on oystercard
+# FAIL
+# Write unit test using raise error
+# create constant upper_limit = 90 for balance
+# implement limit in the top_up method, include upper_limit value
