672acb28047a6faeeae1739fbb486f86fa9e35d0
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-13 15:06:59 +0100

    Chapter 4 added oystercard class added tests for initialization and balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,18 @@
 # Uninitialized constant Oystercard (NameError)
 # ./spec/oystercard_spec.rb:2:in `<top (required)>'
 # line 2
-# Create file and create class Oystercard link to spec file
-describe Oystercard do
+# Create class file oystercard.rb and create class Oystercard, link to spec file
+# try to make instance of Oystercard in irb, oystercard.balance
+# create initialize method with default balance
+# create attr_reader :balance
+require 'oystercard'
 
+describe Oystercard do
+  it "creates an oystercard" do
+    expect(Oystercard.new).to be_a(Oystercard)
+  end
+
+  it "has a starting balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
